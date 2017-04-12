7baeb70a5327aab25318a7df888f23180c764e76
Lanna <Lanna_lmb@hotmail.com>
2017-04-12 11:57:52 -0400

    Initial Commit

---
+++ MethodTest2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetIntegers(5, 4);
+        }
+         public static int GetIntegers(int x, int y)
+        {
+            int result = 1;
+            int input;
+
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("That is Not an Integer!  Please Enter an Integer:  ");
+            }
+
+            for (int i = 0; i < input; i++)
+            {
+
+                result = result * input;
+                Console.WriteLine("The Result is " + result);
+            }
+            return result;
+        }
+        }
+
+    }
